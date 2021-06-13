51278b106dc494f6dfb9f35670f8e748060da958
johannranudd <33978097+johannranudd@users.noreply.github.com>
2021-06-13 22:06:02 +0200

    yeyeyeye

--- js/script.js
+++ js/script.js
@@ -1,147 +1,104 @@
 const menu = [
   {
     id: 1,
-    title: "pancakes",
-    image: "../img/pancakes.jpg",
-    price: "$15",
-    text: "0 Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus optio libero laborum velit. Ipsum assumenda excepturi quae! Nam, eius atque!",
+    title: "buttermilk pancakes",
+    category: "breakfast",
+    price: 15.99,
+    img: "./images/item-1.jpeg",
+    desc: `I'm baby woke mlkshk wolf bitters live-edge blue bottle, hammock freegan copper mug whatever cold-pressed `,
   },
   {
     id: 2,
-    title: "waffles",
-    image: "https://www.tasteandtellblog.com/wp-content/uploads/2020/01/Best-Waffle-Recipe-tasteandtellblog.com-1.jpg",
-    price: "$12",
-    text: "1 Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus optio libero laborum velit. Ipsum assumenda excepturi quae! Nam, eius atque!",
+    title: "diner double",
+    category: "lunch",
+    price: 13.99,
+    img: "./images/item-2.jpeg",
+    desc: `vaporware iPhone mumblecore selvage raw denim slow-carb leggings gochujang helvetica man braid jianbing. Marfa thundercats `,
   },
   {
     id: 3,
-    title: "sandwich",
-    image: "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/190322-ham-sandwich-horizontal-1553721016.png",
-    price: "$20",
-    text: "2 Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus optio libero laborum velit. Ipsum assumenda excepturi quae! Nam, eius atque!",
+    title: "godzilla milkshake",
+    category: "shakes",
+    price: 6.99,
+    img: "./images/item-3.jpeg",
+    desc: `ombucha chillwave fanny pack 3 wolf moon street art photo booth before they sold out organic viral.`,
+  },
+  {
+    id: 4,
+    title: "country delight",
+    category: "breakfast",
+    price: 20.99,
+    img: "./images/item-4.jpeg",
+    desc: `Shabby chic keffiyeh neutra snackwave pork belly shoreditch. Prism austin mlkshk truffaut, `,
+  },
+  {
+    id: 5,
+    title: "egg attack",
+    category: "lunch",
+    price: 22.99,
+    img: "./images/item-5.jpeg",
+    desc: `franzen vegan pabst bicycle rights kickstarter pinterest meditation farm-to-table 90's pop-up `,
+  },
+  {
+    id: 6,
+    title: "oreo dream",
+    category: "shakes",
+    price: 18.99,
+    img: "./images/item-6.jpeg",
+    desc: `Portland chicharrones ethical edison bulb, palo santo craft beer chia heirloom iPhone everyday`,
+  },
+  {
+    id: 7,
+    title: "bacon overflow",
+    category: "breakfast",
+    price: 8.99,
+    img: "./images/item-7.jpeg",
+    desc: `carry jianbing normcore freegan. Viral single-origin coffee live-edge, pork belly cloud bread iceland put a bird `,
+  },
+  {
+    id: 8,
+    title: "american classic",
+    category: "lunch",
+    price: 12.99,
+    img: "./images/item-8.jpeg",
+    desc: `on it tumblr kickstarter thundercats migas everyday carry squid palo santo leggings. Food truck truffaut  `,
+  },
+  {
+    id: 9,
+    title: "quarantine buddy",
+    category: "shakes",
+    price: 16.99,
+    img: "./images/item-9.jpeg",
+    desc: `skateboard fam synth authentic semiotics. Live-edge lyft af, edison bulb yuccie crucifix microdosing.`,
+  },
+  {
+    id: 10,
+    title: "bison steak",
+    category: "dinner",
+    price: 22.99,
+    img: "./images/item-10.jpeg",
+    desc: `skateboard fam synth authentic semiotics. Live-edge lyft af, edison bulb yuccie crucifix microdosing.`,
   },
 ];
 
-let currentItem = 0;
-
-const nextBtn = document.querySelector(".nextBtn");
-const prevBtn = document.querySelector(".prevBtn");
-const itemName = document.querySelector(".item-name");
-const photo = document.querySelector(".photo");
-const price = document.querySelector(".price");
-const text = document.querySelector(".text");
+const sectionCenter = document.querySelector(".section-center");
 
 window.addEventListener("DOMContentLoaded", function () {
-  showMenuItem();
+  let displayMenu = menu.map(function (item) {
+
+    return `<article class="menu-item">
+    <img src=${item.img} class="photo" alt=${item.img} />
+      <div class="item-info">
+          <header>
+          <h4>${item.title}</h4>
+          <h4 class="price">$${item.price}</h4>
+          </header>
+          <p class="item-text">
+          ${item.desc}
+          </p>
+      </div>
+  </article>`;
+  });
+  displayMenu = displayMenu.join("");
+  sectionCenter.innerHTML = displayMenu;
 });
-
-function showMenuItem() {
-  const onDisplay = menu[currentItem];
-  itemName.textContent = onDisplay.title;
-  photo.src = onDisplay.image;
-  price.textContent = onDisplay.price;
-  text.textContent = onDisplay.text;
-}
-
-nextBtn.addEventListener("click", function () {
-  currentItem++;
-
-  if (currentItem > menu.length - 1) {
-    currentItem = 0;
-  }
-  showMenuItem();
-});
-
-prevBtn.addEventListener("click", function () {
-  currentItem--;
-
-  if (currentItem < 0) {
-    currentItem = menu.length - 1;
-  }
-  showMenuItem();
-});
-
-const pancakeBtn = document.querySelector(".pancakeBtn");
-const waffleBtn = document.querySelector(".waffleBtn");
-const sandwichBtn = document.querySelector(".sandwichBtn");
-
-pancakeBtn.addEventListener("click", function () {
-    for (let i = 0; i < menu.length; i++) {
-        if (menu[i].title === 'pancakes') {
-            currentItem = 0;
-            showMenuItem();
-        }
-    }
-});
-waffleBtn.addEventListener("click", function () {
-    for (let i = 0; i < menu.length; i++) {
-        if (menu[i].title === 'waffles') {
-            currentItem = 1;
-            showMenuItem();
-        }
-    }
-});
-sandwichBtn.addEventListener("click", function () {
-    for (let i = 0; i < menu.length; i++) {
-        if (menu[i].title === 'sandwich') {
-            currentItem = 2;
-            showMenuItem();
-        }
-    }
-});
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// console.log(menu[2].title.includes('sandwich'));
-
-// const returnItem = menu.map(function(elem) {
-//     return (elem.title, elem.id+1)
-// })
-
-// console.log(returnItem);
-
-// const rockets = [
-//     { country:'Russia', launches:32 },
-//     { country:'US', launches:23 },
-//     { country:'China', launches:16 },
-//     { country:'Europe(ESA)', launches:7 },
-//     { country:'India', launches:4 },
-//     { country:'Japan', launches:3 }
-// ];
-
-// function check() {
-// for (let i = 0; i < rockets.length; i++) {
-//   let result = rockets[i].launches;
-
-//   if (result === 32) {
-//       console.log(result);
-//       filterBtn.textContent = result;
-//   }
-
-// }
-
-// }
-
-// check();
