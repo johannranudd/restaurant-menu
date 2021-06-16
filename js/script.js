6de9cfa4552629ad63a5f769bf923236db4b621f
johannranudd <33978097+johannranudd@users.noreply.github.com>
2021-06-16 20:26:17 +0200

    done

--- js/script.js
+++ js/script.js
@@ -81,186 +81,46 @@ const menu = [
   },
 ];
 
-const sectionCenter = document.querySelector(".section-center");
-const lunchBtn = document.querySelector(".lunchBtn");
-const breakfastBtn = document.querySelector('.breakfastBtn');
-const shakesBtn = document.querySelector('.shakesBtn');
-const dinnerBtn = document.querySelector('.dinnerBtn');
-const allItems = document.querySelector('.allItems');
+const sectionCenterGrid = document.querySelector(".section-center-grid");
+const filterBtns = document.querySelectorAll(".filter-btn");
 
+// return object to DOM
+function displayMenuItems(menuItem) {
+  let displayItems = menuItem.map(function (item) {
+    return `<aside class="menu-item">
+    <img src=${item.img} class="photo" alt="menu item" />
+    <div class="menu-item-text-section">
+      <header>
+        <h4>${item.title}</h4>
+        <p><strong>$${item.price}</strong></p>
+      </header>
+      <p class="description-text">
+      ${item.desc}
+      </p>
+    </div>
+  </aside>`;
+  });
+  displayItems = displayItems.join("");
+  sectionCenterGrid.innerHTML = displayItems;
+}
 
+// on Load
 window.addEventListener("DOMContentLoaded", function () {
-  displayMenuItems();
+  displayMenuItems(menu);
 });
 
-function displayMenuItems() {
-  let displayMenu = menu.map(function (item) {
-    return `<article class="menu-item">
-    <img src=${item.img} class="photo" alt=${item.img} />
-      <div class="item-info">
-          <header>
-          <h4>${item.title}</h4>
-          <h4 class="price">$${item.price}</h4>
-          </header>
-          <p class="item-text">
-          ${item.desc}
-          </p>
-      </div>
-  </article>`;
+// choose categoies
+
+filterBtns.forEach(function (btn) {
+  btn.addEventListener("click", function (e) {
+    const buttonId = e.currentTarget.dataset.id;
+    const menuCategory = menu.filter(function (menuItem) {
+      if (buttonId == "all") {
+        return buttonId;
+      } else if (menuItem.category === buttonId) {
+        return menuItem.category;
+      }
+    });
+    displayMenuItems(menuCategory);
   });
-  displayMenu = displayMenu.join("");
-  sectionCenter.innerHTML = displayMenu;
-}
-
-// All items
-
-allItems.addEventListener("click", function() {
-  displayMenuItems();
-})
-
-// Lunch
-
-function lunchSearch() {
-  let displayLunch = menu.map(function (lunch) {
-    if (lunch.category === "lunch") {
-      // return lunch.category;
-      return `<article class="menu-item">
-        <img src=${lunch.img} class="photo" alt=${lunch.img} />
-        <div class="item-info">
-            <header>
-            <h4>${lunch.title}</h4>
-            <h4 class="price">$${lunch.price}</h4>
-            </header>
-            <p class="item-text">
-            ${lunch.desc}
-            </p>
-        </div>
-    </article>`;
-    }
-  });
-  displayLunch = displayLunch.join("");
-  sectionCenter.innerHTML = displayLunch;
-}
-
-lunchBtn.addEventListener("click", function () {
-  lunchSearch();
-});
-
-
-// breakfast
-
-
-function breakfastSearch() {
-  let displayBreakfast = menu.map(function(breakfast) {
-
-    if (breakfast.category === "breakfast") {
-      // console.log(breakfast);
-      return `<article class="menu-item">
-      <img src=${breakfast.img} class="photo" alt=${breakfast.img} />
-      <div class="item-info">
-          <header>
-          <h4>${breakfast.title}</h4>
-          <h4 class="price">$${breakfast.price}</h4>
-          </header>
-          <p class="item-text">
-          ${breakfast.desc}
-          </p>
-      </div>
-  </article>`;
-   };
-  });
-  displayBreakfast = displayBreakfast.join("");
-  sectionCenter.innerHTML = displayBreakfast;
-}
-
-breakfastBtn.addEventListener('click', function() {
-  breakfastSearch()
-});
-
-
-// shakes
-
-function shakesSearch() {
-  let displayShakes = menu.map(function(shakes) {
-
-    if (shakes.category === "shakes") {
-      // console.log(breakfast);
-      return `<article class="menu-item">
-      <img src=${shakes.img} class="photo" alt=${shakes.img} />
-      <div class="item-info">
-          <header>
-          <h4>${shakes.title}</h4>
-          <h4 class="price">$${shakes.price}</h4>
-          </header>
-          <p class="item-text">
-          ${shakes.desc}
-          </p>
-      </div>
-  </article>`;
-   };
-  });
-  displayShakes = displayShakes.join("");
-  sectionCenter.innerHTML = displayShakes;
-}
-
-shakesBtn.addEventListener('click', function() {
-  shakesSearch()
-});
-
-// dinner
-
-function dinnerSearch() {
-  let displayDinner = menu.map(function(dinner) {
-
-    if (dinner.category === "dinner") {
-      // console.log(breakfast);
-      return `<article class="menu-item">
-      <img src=${dinner.img} class="photo" alt=${dinner.img} />
-      <div class="item-info">
-          <header>
-          <h4>${dinner.title}</h4>
-          <h4 class="price">$${dinner.price}</h4>
-          </header>
-          <p class="item-text">
-          ${dinner.desc}
-          </p>
-      </div>
-  </article>`;
-   };
-  });
-  displayDinner = displayDinner.join("");
-  sectionCenter.innerHTML = displayDinner;
-}
-
-dinnerBtn.addEventListener('click', function() {
-  dinnerSearch();
-});
-
-// search by click
-
-const searchBtn = document.querySelector('.searchBtn');
-const searchInput = document.querySelector('.search-input');
-
-function searchFunction() {
-  if (searchInput.value.toUpperCase() === "DINNER") {
-    dinnerSearch();
-  } else if (searchInput.value.toUpperCase() === "LUNCH") {
-    lunchSearch();
-  } else if (searchInput.value.toUpperCase() === "SHAKES") {
-    shakesSearch();
-  } else if (searchInput.value.toUpperCase() === "BREAKFAST") {
-    breakfastSearch();
-  } 
-};
-
-searchBtn.addEventListener('click', function() {
-  searchFunction();
-});
-
-// search by keypress enter
-
-window.addEventListener('keypress', function(enter) {
-  if (enter.keyCode == 13) {
-    searchFunction();
-  }
 });
